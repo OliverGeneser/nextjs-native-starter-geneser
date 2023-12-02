d0049b56a58c6b89e61a7dccfd323440cbf2f896
OliverGeneser <32340667+OliverGeneser@users.noreply.github.com>
2023-12-02 18:48:53 +0100

    Changed urls in config

--- packages/lib/utils/config.ts
+++ packages/lib/utils/config.ts
@@ -1,6 +1,6 @@
 import { AuthMode } from './common.types'
 
-export const PROD_URL_NO_HTTPS = 'nextjs-native-starter-app.vercel.app'
+export const PROD_URL_NO_HTTPS = 'nextjs-native-starter-geneser-app.vercel.app'
 
 export const IS_IN_PRODUCTION_ENVIRONMENT =
   process.env.NEXT_PUBLIC_APP_STAGE !== 'development'
@@ -14,7 +14,7 @@ export const APP_BUNDLE_URL = `next.supertokens.app` // used to create the deep 
 
 export const REDIRECT_URL = !IS_IN_PRODUCTION_ENVIRONMENT
   ? FRONTEND_URL
-  : 'https://nextjs-native-starter-app-git-main-robschilderr.vercel.app'
+  : 'https://nextjs-native-starter-geneser-app-git-main-olivergeneser.vercel.app'
 
 export const APP_ORIGIN_URLS = [
   'capacitor://localhost',
@@ -34,7 +34,7 @@ export const ALLOWED_CORS_URLS = [
   'http://localhost:3001',
   'https://www.nextjs-native.dev',
   'https://nextjs-native.dev',
-  'https://nextjs-native-git-main-robschilderr.vercel.app',
+  'https://nextjs-native-geneser-git-main-olivergeneser.vercel.app',
   'http://localhost',
 ]
 
